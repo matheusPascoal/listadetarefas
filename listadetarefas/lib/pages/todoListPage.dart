62ecd2c6b6f6698f471f177bc7e1f79298fab262
matheusPascoal <pascoalleander@gmail.com>
2022-03-13 02:03:39 -0300

    ListView Adicionado

--- listadetarefas/lib/pages/todoListPage.dart
+++ listadetarefas/lib/pages/todoListPage.dart
@@ -8,37 +8,83 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
-  final TextEditingController emailControler = TextEditingController();
-
+  TextEditingController todosController = TextEditingController();
+  List<String> todos = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  // O CONTROLLER VINCULA O CAMPO DE TEXTO COM O CONTROLER
-                  controller: emailControler,
-                  decoration: InputDecoration(labelText: 'Email'),
-                  onChanged: onChanged,
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'Lista de Tarefas',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 30),
                 ),
-                ElevatedButton(onPressed: login, child: Text('entrar'))
-              ],
-            ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: todosController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        // VE OQUE ESTA DENTRO DO CAMPO COM O CONTROLER
+                        String text = todosController.text;
+                        setState(() {
+                          todos.add(text);
+                        });
+                        todosController.clear();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0XFF01EEFF),
+                        padding: EdgeInsets.all(12),
+                      ),
+                      child: Icon(
+                        Icons.add,
+                        size: 50,
+                      )),
+                ],
+              ),
+              Flexible(
+                  child: ListView(
+                shrinkWrap: true,
+                children: [
+                  // ESSE FOR DIZ QUE QUER AS INFOS DE TAREFA ATE AS TAREFAS
+                  // DE PONTA A PONTA
+                  for (String todo in todos)
+                    ListTile(
+                      //CHAMA O TEXTO QUE ESTA DENTRO DA LISTA
+                      title: Text('Tarefa: $todo'),
+                      leading: Icon(Icons.save),
+                    )
+                ],
+              )),
+              Row(
+                children: [
+                  Expanded(child: Text('Você possui 0 tarefas pendentes')),
+                  ElevatedButton(onPressed: () {}, child: Text('Limpar tudo'))
+                ],
+              )
+            ],
           ),
-        ));
-  }
-
-  login() {
-    String text = emailControler.text;
-    print('text');
-  }
-
-  onChanged(String text) {
-    print('text');
+        ),
+      ),
+    );
   }
 }
